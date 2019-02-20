514c0ef43b2dc29ddfda1c710070fe63dff989b1
muxing <log_e@qq.com>
2019-02-20 21:48:48 +0800

    ready article view, post, delete

--- myblog/views.py
+++ myblog/views.py
@@ -4,28 +4,65 @@ from flask_login import login_user, login_required, logout_user, current_user
 from .models import User, Article
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if not current_user.is_authenticated:
-            return redirect(url_for('index'))
-        title = request.form
+    #todo
     return render_template('index.html')
 
 
+@app.route('/article/delete/<int:article_id>')
+@login_required
+def delete_article(article_id):
+    article = Article.query.get_or_404(article_id)
+    db.session.delete(article)
+    db.session.commit()
+    flash("delete article success")
+    return redirect(url_for("admin"))
+
+
 @app.route('/article/view/<int:article_id>')
-def view_article(article):
-    pass
+def view_article(article_id):
+    article = Article.query.get(article_id)
+    return render_template('article.html', article = article)
 
 
 @app.route('/article/edit/<int:article_id>')
 @login_required
-def edit_article(article):
+def edit_article(article_id):
+    """ 编辑已有的文章 """
+    #todo
+    article = Article.query.get(article_id)
     pass
+
+
+@app.route('/article/post', methods=['GET','POST'])
+@login_required
+def post_article():
+    """ 发布新文章 """
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title or not content:
+            flash('Empty article')
+            redirect(url_for('post_article'))
+        article = Article(title=title, content = content)
+        db.session.add(article)
+        db.session.commit()
+        flash('Article create successfully!')
+        return redirect(url_for('admin'))
+
+
+@app.route('/admin')
+@login_required
+def admin():
+    """ 管理员页面，可以编辑旧文章，发布新文章，删除旧文章 """
+    #todo
+    return render_template('admin.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """ 登录 admin 页面 """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -39,7 +76,7 @@ def login():
         if username == user.username and user.check_password(password):
             login_user(user)
             flash('Login Success')
-            return redirect(url_for('index'))
+            return redirect(url_for('admin'))
         
         flash('Invalid username or password.')
         return redirect(url_for('login'))
